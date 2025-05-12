4d5ec5d7f686fc0ac0af331d0f8cfc8fcf0656ed
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-12 21:21:38 +0530

    Add Lombok annotations to Account and User models for boilerplate reduction

--- backend/src/main/java/com/edupilot/backend/model/Account.java
+++ backend/src/main/java/com/edupilot/backend/model/Account.java
@@ -3,9 +3,15 @@ package com.edupilot.backend.model;
 import com.edupilot.backend.model.base.AuditCreation;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "account")
 public class Account extends AuditCreation {
 
