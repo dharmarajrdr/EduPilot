e605699d6a62fb9ea4e28774bf3b8789778a37fd
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-12 23:01:55 +0530

    Add global exception handling for permission and user-related errors

--- backend/src/main/java/com/edupilot/backend/util/GlobalExceptionHandler.java
+++ backend/src/main/java/com/edupilot/backend/util/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.edupilot.backend.util;
 
-import com.edupilot.backend.custom_exception.AccountAlreadyRegistered;
+import com.edupilot.backend.custom_exception.*;
 import com.edupilot.backend.dto.response.BaseResponseDto;
 import com.edupilot.backend.model.enums.ResponseStatus;
 import org.springframework.http.HttpStatus;
@@ -15,6 +15,41 @@ public class GlobalExceptionHandler {
     public ResponseEntity<BaseResponseDto> accountAlreadyRegistered(AccountAlreadyRegistered e) {
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                BaseResponseDto.builder().message(e.getMessage()).status(ResponseStatus.FAILURE).build()
+        );
+    }
+
+    @ExceptionHandler(PermissionDenied.class)
+    public ResponseEntity<BaseResponseDto> permissionDenied(PermissionDenied e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
+                BaseResponseDto.builder().message(e.getMessage()).status(ResponseStatus.FAILURE).build()
+        );
+    }
+
+    @ExceptionHandler(InstructorNotFound.class)
+    public ResponseEntity<BaseResponseDto> instructorNotFound(InstructorNotFound e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                BaseResponseDto.builder().message(e.getMessage()).status(ResponseStatus.FAILURE).build()
+        );
+    }
+
+    @ExceptionHandler(UserNotFound.class)
+    public ResponseEntity<BaseResponseDto> userNotFound(UserNotFound e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                BaseResponseDto.builder().message(e.getMessage()).status(ResponseStatus.FAILURE).build()
+        );
+    }
+
+    @ExceptionHandler(UserNameAlreadyExist.class)
+    public ResponseEntity<BaseResponseDto> userNameAlreadyExist(UserNameAlreadyExist e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                BaseResponseDto.builder().message(e.getMessage()).status(ResponseStatus.FAILURE).build()
+        );
+    }
+
+    @ExceptionHandler(NewAdminCreationRestricted.class)
+    public ResponseEntity<BaseResponseDto> newAdminCreationRestricted(NewAdminCreationRestricted e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
                 BaseResponseDto.builder().message(e.getMessage()).status(ResponseStatus.FAILURE).build()
         );
     }
