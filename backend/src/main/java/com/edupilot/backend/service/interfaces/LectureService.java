cf1550d76f22d24d3846751e683d4fbb64cfba6b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-13 13:22:34 +0530

    Add deleteLecture method to LectureService and delete method to VideoService for handling deletions

--- backend/src/main/java/com/edupilot/backend/service/interfaces/LectureService.java
+++ backend/src/main/java/com/edupilot/backend/service/interfaces/LectureService.java
@@ -1,10 +1,13 @@
 package com.edupilot.backend.service.interfaces;
 
 import com.edupilot.backend.dto.request.CreateLectureRequestDto;
+import com.edupilot.backend.dto.request.DeleteLectureRequestDto;
 import com.edupilot.backend.dto.response.CreateLectureResponseDto;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface LectureService {
 
     public CreateLectureResponseDto addLecture(MultipartFile video, CreateLectureRequestDto createLectureRequestDto, Long userId);
+
+    public void deleteLecture(DeleteLectureRequestDto deleteLectureRequestDto, Long userId);
 }
