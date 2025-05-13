ab7eb8b9858e7f2b277e42f4e3699c14536999e8
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-13 13:22:00 +0530

    Add methods to ResponseStatus enum for success and failure checks

--- backend/src/main/java/com/edupilot/backend/model/enums/ResponseStatus.java
+++ backend/src/main/java/com/edupilot/backend/model/enums/ResponseStatus.java
@@ -2,5 +2,13 @@ package com.edupilot.backend.model.enums;
 
 public enum ResponseStatus {
 
-    SUCCESS, FAILURE
+    SUCCESS, FAILURE;
+
+    public Boolean isSuccess() {
+        return this == ResponseStatus.SUCCESS;
+    }
+
+    public Boolean isFailure() {
+        return this == ResponseStatus.FAILURE;
+    }
 }
