c453e22fd65ede8427d406664f649002ce2eb91d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-13 13:22:06 +0530

    Add method to LectureRepository for finding lecture by ID and course

--- backend/src/main/java/com/edupilot/backend/repository/LectureRepository.java
+++ backend/src/main/java/com/edupilot/backend/repository/LectureRepository.java
@@ -1,9 +1,14 @@
 package com.edupilot.backend.repository;
 
+import com.edupilot.backend.model.Course;
 import com.edupilot.backend.model.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
+
+    Optional<Lecture> findLectureByIdAndCourse(Long id, Course course);
 }
