0ee24474875d183a313701929a1ec0a16cb5af66
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-13 08:56:22 +0530

    Add publishCourse endpoint to CourseController for publishing courses

--- backend/src/main/java/com/edupilot/backend/controller/CourseController.java
+++ backend/src/main/java/com/edupilot/backend/controller/CourseController.java
@@ -1,8 +1,10 @@
 package com.edupilot.backend.controller;
 
 import com.edupilot.backend.dto.request.CreateCourseRequestDto;
+import com.edupilot.backend.dto.request.EditCourseRequestDto;
 import com.edupilot.backend.dto.response.BaseResponseDto;
 import com.edupilot.backend.dto.response.CreateCourseResponseDto;
+import com.edupilot.backend.model.enums.CourseStatus;
 import com.edupilot.backend.model.enums.ResponseStatus;
 import com.edupilot.backend.service.interfaces.CourseService;
 import lombok.AllArgsConstructor;
@@ -10,14 +12,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
 @AllArgsConstructor
-@RequestMapping("/v1/api")
+@RequestMapping("/v1/api/course")
 public class CourseController {
 
     private final CourseService courseService;
 
-    @PostMapping("/course")
+    @PostMapping("")
     public ResponseEntity<BaseResponseDto> createCourse(@RequestBody CreateCourseRequestDto createCourseRequestDto, @RequestParam Long userId) {
 
         CreateCourseResponseDto createCourseResponseDto = courseService.createCourse(createCourseRequestDto, userId);
@@ -27,5 +31,21 @@ public class CourseController {
                 .data(createCourseResponseDto)
                 .build();
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @PatchMapping("/publish/{courseId}")
+    public ResponseEntity<BaseResponseDto> publishCourse(@PathVariable Long courseId, @RequestParam Long userId) {
+
+        EditCourseRequestDto editCourseRequestDto = EditCourseRequestDto.builder()
+                .id(courseId)
+                .status(CourseStatus.PUBLISHED)
+                .releasedDate(LocalDateTime.now())
+                .build();
+        courseService.patchCourse(editCourseRequestDto, userId);
+        BaseResponseDto responseDto = BaseResponseDto.builder()
+                .message("Course published successfully.")
+                .status(ResponseStatus.SUCCESS)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 }
