0a37fc5779d48a38557ee627b97cb51112fde278
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-13 13:22:18 +0530

    Implement deleteLecture method in LectureService for lecture deletion functionality

--- backend/src/main/java/com/edupilot/backend/service/implementation/LectureServiceImpl.java
+++ backend/src/main/java/com/edupilot/backend/service/implementation/LectureServiceImpl.java
@@ -1,7 +1,12 @@
 package com.edupilot.backend.service.implementation;
 
+import com.edupilot.backend.custom_exception.FlixifyVideoDeleteException;
+import com.edupilot.backend.custom_exception.LectureNotFound;
 import com.edupilot.backend.dto.request.CreateLectureRequestDto;
+import com.edupilot.backend.dto.request.DeleteLectureRequestDto;
+import com.edupilot.backend.dto.request.VideoDeleteRequestDto;
 import com.edupilot.backend.dto.response.CreateLectureResponseDto;
+import com.edupilot.backend.dto.response.VideoDeleteResponseDto;
 import com.edupilot.backend.dto.response.VideoUploadResponseDto;
 import com.edupilot.backend.model.Course;
 import com.edupilot.backend.model.Lecture;
@@ -13,6 +18,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -41,5 +48,39 @@ public class LectureServiceImpl implements LectureService {
         lecture.setVideoId(videoUploadResponseDto.getVideoId());
 
         return CreateLectureResponseDto.fromLecture(lectureRepository.save(lecture));
+    }
+
+    /**
+     * Find the lecture in given course
+     *
+     * @param lectureId
+     * @param course
+     * @return
+     */
+    private Lecture findLectureByIdAndCourse(Long lectureId, Course course) {
+
+        return lectureRepository.findLectureByIdAndCourse(lectureId, course).orElseThrow(() -> new LectureNotFound(lectureId, course.getId()));
+    }
+
+    /**
+     * Delete the given lecture in the course
+     *
+     * @param deleteLectureRequestDto
+     * @param userId
+     */
+    @Override
+    public void deleteLecture(DeleteLectureRequestDto deleteLectureRequestDto, Long userId) {
+
+        Course course = courseService.getCourseById(deleteLectureRequestDto.getCourseId());
+        Lecture lecture = findLectureByIdAndCourse(deleteLectureRequestDto.getLectureId(), course);
+        UUID videoId = lecture.getVideoId();
+        VideoDeleteRequestDto videoDeleteRequestDto = VideoDeleteRequestDto.builder().videoId(videoId).build();
+
+        VideoDeleteResponseDto videoDeleteResponseDto = videoService.delete(videoDeleteRequestDto);
+        if (videoDeleteResponseDto.getStatus().isFailure()) {
+            throw new FlixifyVideoDeleteException(videoDeleteResponseDto.getMessage());
+        } else {
+            lectureRepository.delete(lecture);
+        }
     }
 }
