7c99abf2c4296791bd1380b00eedd021b2b42bc7
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-13 18:55:33 +0530

    Refactor createLecture method to accept metadata as a JSON string and update application properties for Hibernate to use 'update' strategy

--- backend/src/main/java/com/edupilot/backend/controller/LectureController.java
+++ backend/src/main/java/com/edupilot/backend/controller/LectureController.java
@@ -6,6 +6,8 @@ import com.edupilot.backend.dto.response.BaseResponseDto;
 import com.edupilot.backend.dto.response.CreateLectureResponseDto;
 import com.edupilot.backend.model.enums.ResponseStatus;
 import com.edupilot.backend.service.interfaces.LectureService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,32 +23,19 @@ public class LectureController {
     private final LectureService lectureService;
 
     @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<BaseResponseDto> createLecture(
-            @RequestPart CreateLectureRequestDto createLectureRequestDto,
-            @RequestPart("file") MultipartFile file,
-            @RequestParam Long userId
-    ) {
+    public ResponseEntity<BaseResponseDto> createLecture(@RequestPart("meta") String meta, @RequestPart("file") MultipartFile file, @RequestParam Long userId) throws JsonProcessingException {
 
+        CreateLectureRequestDto createLectureRequestDto = new ObjectMapper().readValue(meta, CreateLectureRequestDto.class);
         CreateLectureResponseDto createLectureResponseDto = lectureService.addLecture(file, createLectureRequestDto, userId);
-        BaseResponseDto baseResponseDto = BaseResponseDto.builder()
-                .message("Lecture created successfully.")
-                .status(ResponseStatus.SUCCESS)
-                .data(createLectureResponseDto)
-                .build();
+        BaseResponseDto baseResponseDto = BaseResponseDto.builder().message("Lecture created successfully.").status(ResponseStatus.SUCCESS).data(createLectureResponseDto).build();
         return ResponseEntity.status(HttpStatus.CREATED).body(baseResponseDto);
     }
 
     @DeleteMapping("")
-    public ResponseEntity<BaseResponseDto> deleteLecture(
-            @RequestBody DeleteLectureRequestDto deleteLectureRequestDto,
-            @RequestParam Long userId
-    ) {
+    public ResponseEntity<BaseResponseDto> deleteLecture(@RequestBody DeleteLectureRequestDto deleteLectureRequestDto, @RequestParam Long userId) {
 
         lectureService.deleteLecture(deleteLectureRequestDto, userId);
-        BaseResponseDto baseResponseDto = BaseResponseDto.builder()
-                .message("Lecture deleted successfully")
-                .status(ResponseStatus.SUCCESS)
-                .build();
+        BaseResponseDto baseResponseDto = BaseResponseDto.builder().message("Lecture deleted successfully").status(ResponseStatus.SUCCESS).build();
         return ResponseEntity.status(HttpStatus.OK).body(baseResponseDto);
     }
 }
