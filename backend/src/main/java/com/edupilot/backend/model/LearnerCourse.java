349877ce0a41f9614ed579bee493d299df7fc254
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-14 07:34:46 +0530

    Add LearnerCourse entity and repository for course enrollment management

--- backend/src/main/java/com/edupilot/backend/model/LearnerCourse.java
+++ backend/src/main/java/com/edupilot/backend/model/LearnerCourse.java
@@ -3,11 +3,15 @@ package com.edupilot.backend.model;
 import com.edupilot.backend.model.base.AuditCreation;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class LearnerCourse extends AuditCreation {
 
