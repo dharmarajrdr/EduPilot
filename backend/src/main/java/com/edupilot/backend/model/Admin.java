1e497ce0dcbcfbf2ff962458a7f58ab384acfe4c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-14 12:14:13 +0530

    Enhance Admin model and AdminService interface with addedBy relationship and instructor approval/rejection methods

--- backend/src/main/java/com/edupilot/backend/model/Admin.java
+++ backend/src/main/java/com/edupilot/backend/model/Admin.java
@@ -1,16 +1,27 @@
 package com.edupilot.backend.model;
 
 import com.edupilot.backend.model.base.AuditCreation;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 @Entity(name = "admin")
 public class Admin extends AuditCreation {
 
     @OneToOne
     private User user;
+
+    @ManyToOne
+    private Admin addedBy;
 }
--- backend/src/main/java/com/edupilot/backend/service/interfaces/AdminService.java
+++ backend/src/main/java/com/edupilot/backend/service/interfaces/AdminService.java
@@ -1,8 +1,15 @@
 package com.edupilot.backend.service.interfaces;
 
+import com.edupilot.backend.dto.request.RejectInstructorRequestDto;
 import com.edupilot.backend.model.Admin;
 
 public interface AdminService {
 
     public Admin getAdmin();
+
+    public void approveInstructor(Long instructorId, Long adminId);
+
+    public void rejectInstructor(Long instructorId, Long adminId, RejectInstructorRequestDto rejectInstructorRequestDto);
+
+    public void save(Admin admin);
 }
