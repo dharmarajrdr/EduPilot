8da9150ca629ad976fcb7f8ac4fe24184a2a5049
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-18 19:49:06 +0530

    Enhance SignupUserRequestDto with builder pattern and conversion method from User

--- backend/src/main/java/com/edupilot/backend/dto/request/SignupUserRequestDto.java
+++ backend/src/main/java/com/edupilot/backend/dto/request/SignupUserRequestDto.java
@@ -3,9 +3,15 @@ package com.edupilot.backend.dto.request;
 import com.edupilot.backend.model.Account;
 import com.edupilot.backend.model.User;
 import com.edupilot.backend.model.enums.UserType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignupUserRequestDto {
 
     private String email;
@@ -23,5 +29,17 @@ public class SignupUserRequestDto {
     public User toUser() {
         Account account = Account.builder().email(email).phoneNumber(phoneNumber).password(password).build();
         return User.builder().account(account).userType(userType).displayName(displayName).userName(userName).build();
+    }
+
+    public static SignupUserRequestDto fromUser(User user) {
+
+        return SignupUserRequestDto.builder()
+                .userName(user.getUserName())
+                .userType(user.getUserType())
+                .displayName(user.getDisplayName())
+                .email(user.getAccount().getEmail())
+                .password(user.getAccount().getPassword())
+                .phoneNumber(user.getAccount().getPhoneNumber())
+                .build();
     }
 }
