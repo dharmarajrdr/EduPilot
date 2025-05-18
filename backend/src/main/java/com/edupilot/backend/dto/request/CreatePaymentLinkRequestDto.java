dc2652ab0cf9a77b4e6f885e84616c6268cec69b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-18 11:13:16 +0530

    Added dto to request payment link based on orderId

---
+++ backend/src/main/java/com/edupilot/backend/dto/request/CreatePaymentLinkRequestDto.java
@@ -0,0 +1,9 @@
+package com.edupilot.backend.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CreatePaymentLinkRequestDto {
+
+    private Long orderId;
+}
---
+++ backend/src/main/java/com/edupilot/backend/dto/response/CreatePaymentLinkResponseDto.java
@@ -0,0 +1,9 @@
+package com.edupilot.backend.dto.response;
+
+import lombok.Data;
+
+@Data
+public class CreatePaymentLinkResponseDto {
+
+    private String url;
+}
