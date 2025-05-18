663e037b61177121995ff94db53b0d7cea808684
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-18 11:11:44 +0530

    Added blackbox

---
+++ backend/src/main/java/com/edupilot/backend/machine_learning/PaymentServiceAnalyser.java
@@ -0,0 +1,17 @@
+package com.edupilot.backend.machine_learning;
+
+import com.edupilot.backend.model.enums.PaymentType;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceAnalyser {
+
+    /***
+     * This method is responsible to return the better performant payment service at that moment.
+     * @return PaymentType
+     */
+    public PaymentType getBetterPerformantPaymentType() {
+
+        return null;
+    }
+}
