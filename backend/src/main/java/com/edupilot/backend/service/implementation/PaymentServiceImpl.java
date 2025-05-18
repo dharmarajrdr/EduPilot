c9edc82b7b421852c9bc5333ce939e99a9fff418
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-18 11:14:10 +0530

    Added payment link generation task

---
+++ backend/src/main/java/com/edupilot/backend/service/implementation/PaymentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.edupilot.backend.service.implementation;
+
+import org.springframework.stereotype.Service;
+
+import com.edupilot.backend.machine_learning.PaymentServiceAnalyser;
+import com.edupilot.backend.model.Order;
+import com.edupilot.backend.model.enums.PaymentType;
+import com.edupilot.backend.registry.PaymentServiceRegistry;
+import com.edupilot.backend.service.interfaces.OrderService;
+import com.edupilot.backend.service.interfaces.PaymentService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class PaymentServiceImpl {
+
+    private final PaymentServiceAnalyser paymentServiceAnalyser;
+    private final PaymentServiceRegistry paymentServiceRegistry;
+    private final OrderService orderService;
+
+    /**
+     * Generate the payment link based on ML analyser
+     *
+     * @param orderId
+     * @return String
+     */
+    public String createPaymentLink(Long orderId) {
+
+        // 1. Get order info from orderService
+        Order order = orderService.getOrderById(orderId);
+        Long price = order.getPrice();
+
+        // 2. Get better performant payment service
+        PaymentType paymentType = paymentServiceAnalyser.getBetterPerformantPaymentType();
+
+        // 3. Get payment service object based on strategy
+        PaymentService paymentService = paymentServiceRegistry.getPaymentService(paymentType);
+
+        // 4. Create payment link
+        return paymentService.createPaymentLink(price);
+    }
+}
---
+++ backend/src/main/java/com/edupilot/backend/service/interfaces/PaymentService.java
@@ -0,0 +1,6 @@
+package com.edupilot.backend.service.interfaces;
+
+public interface PaymentService {
+
+    public String createPaymentLink(Long price);
+}
