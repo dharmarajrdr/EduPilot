40db75b9597b2021f9855db281637e30d605f0a8
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-20 13:37:29 +0530

    Enhance CategoryService with caching and CRUD operations
    - Add findCategoryById method to retrieve category by ID with caching
    - Implement deleteCategoryById method to remove category by ID with cache eviction
    - Update save method to cache newly created categories

--- backend/src/main/java/com/edupilot/backend/service/implementation/CategoryServiceImpl.java
+++ backend/src/main/java/com/edupilot/backend/service/implementation/CategoryServiceImpl.java
@@ -1,9 +1,13 @@
 package com.edupilot.backend.service.implementation;
 
+import com.edupilot.backend.custom_exception.CategoryNotFound;
 import com.edupilot.backend.model.Category;
 import com.edupilot.backend.repository.CategoryRepository;
 import com.edupilot.backend.service.interfaces.CategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +17,13 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
 
     /**
+     * Save the new category
+     *
      * @param category
      * @return
      */
     @Override
+    @CachePut(value = "CATEGORY", key = "#result.id()")
     public Category save(Category category) {
 
         String name = category.getName();
@@ -24,15 +31,33 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     /**
+     * Get the category by id
+     *
+     * @param categoryId
+     * @return
+     */
+    @Override
+    @Cacheable(value = "CATEGORY", key = "#categoryId")
+    public Category findCategoryById(Long categoryId) {
+
+        return categoryRepository.findCategoryById(categoryId).orElseThrow(() -> new CategoryNotFound(categoryId));
+    }
+
+    /**
+     * Find the category by name
+     *
      * @param categoryName
      * @return
      */
     @Override
     public Category findCategoryByName(String categoryName) {
+
         return null;
     }
 
     /**
+     * Check whether the category exist of not
+     *
      * @param categoryName
      * @return
      */
@@ -40,5 +65,17 @@ public class CategoryServiceImpl implements CategoryService {
     public Boolean existCategoryByName(String categoryName) {
 
         return categoryRepository.existsByName(categoryName);
+    }
+
+    /**
+     * Delete category based on id
+     *
+     * @param categoryId
+     */
+    @Override
+    @CacheEvict(value = "CATEGORY", key = "#categoryId")
+    public void deleteCategoryById(Long categoryId) {
+
+        categoryRepository.deleteById(categoryId);
     }
 }
